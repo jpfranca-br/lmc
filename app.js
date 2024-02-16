982402ac92b3e93316510876250b4f62c5e0c21b
jpfranca-br <135433891+jpfranca-br@users.noreply.github.com>
2024-02-16 20:42:55 -0300

    V2
    SMART CONTRACT
    - ProductIn, ProductOut and ProductStock data stored as an array, per date
    - Function to check if user already submitted data for specific date
    - Function to allow user to read own data
    - Function to allow owner to read all users data - can pass user address as input parameter or blank (all users)
    
    FRONT END
    - new table to include data
    - better error handling
    
    TO DO
    - frontend for auditing (contract owner-only access)

--- app.js
+++ app.js
@@ -1,231 +1,268 @@
-window.addEventListener('load', async () => {
-    if (window.ethereum) {
+let web3;
+let contract;
+
+async function connectWallet() {
+    if (typeof window.ethereum !== 'undefined') {
         try {
-            await window.ethereum.request({
+            const accounts = await ethereum.request({
                 method: 'eth_requestAccounts'
             });
-            window.web3 = new Web3(window.ethereum);
-            initApp();
+            if (accounts.length === 0) {
+                console.log("No accounts connected");
+                alert("Nenhuma conta conectada.");
+            } else {
+                console.log("Connected accounts:", accounts);
+                document.getElementById("errorMessage").textContent = "Contas disponíveis: " + JSON.stringify(accounts);
+                web3 = new Web3(window.ethereum);
+                loadContract();
+                console.log("Contract loaded");
+            }
         } catch (error) {
-            console.error("User denied account access or an error occurred:", error);
-            var errorMessage = "Usuario negou acesso a conta ou ocorreu um erro: " + (error.message || JSON.stringify(error));
-            document.getElementById("errorMessage").textContent = errorMessage;
-            //document.getElementById("errorMessage").textContent = "Usuario negou acesso a conta ou ocorreu um erro";
-            document.getElementById("errorModal").style.display = "block";
+            console.error("User denied account access");
+            alert("Sistema sem acesso à sua conta. Verifique se Metamask está desbloqueado. Dê refresh na página.");
         }
-    } else if (window.web3) {
-        window.web3 = new Web3(web3.currentProvider);
-        initApp();
     } else {
-        console.error('Non-Ethereum browser detected. You should consider trying MetaMask!');
-        document.getElementById("errorMessage").textContent = "Browser sem extensao Ethereum. Considere instalar o https://metamask.io";
-        document.getElementById("errorModal").style.display = "block";
+        console.error("MetaMask is not installed");
+        alert("Baixe https://metamask.io e crie/importe uma conta na Testnet Sepolia. Depois disso, dê refresh na página.");
     }
+}
+
+const loadContract = async () => {
+    try {
+        // Fetch the ABI
+        const abiResponse = await axios.get('contractABI.json');
+        const abi = abiResponse.data;
+        if (!abi) {
+            alert("Erro técnico. Contate o suporte.");
+            throw new Error("Failed to load contract ABI");
+        }
+        // Fetch the contract address
+        const addressResponse = await axios.get('contractAddress.txt');
+        const address = addressResponse.data.trim();
+        if (!address) {
+            alert("Erro tecnico. Contate o suporte.");
+            throw new Error("Failed to load contract address");
+        }
+        // Create the contract instance
+        contract = new web3.eth.Contract(abi, address);
+        return contract; // Return the contract instance
+    } catch (error) {
+        alert("Erro tecnico. Contate o suporte.");
+        console.error("Error in loadContract:", error);
+        throw error; // Rethrow the error for handling in calling function
+    }
+};
+
+
+function addRow() {
+    var table = document.getElementById("transactionsTableInput");
+    var newRow = table.insertRow(-1);
+
+    var cell0 = newRow.insertCell(0);
+    var cell1 = newRow.insertCell(1);
+    var cell2 = newRow.insertCell(2);
+    var cell3 = newRow.insertCell(3);
+    var cell4 = newRow.insertCell(4);
+    var cell5 = newRow.insertCell(5);
+    var cell6 = newRow.insertCell(6);
+    var cell7 = newRow.insertCell(7);
+    var cell8 = newRow.insertCell(8);
+
+    cell0.innerHTML = '<select class="transactionType" style="width: 70px;text-align: center;"><option value="0">Entrada</option><option value="1">Venda</option><option value="2">Estoque</option></select>';
+    cell1.innerHTML = '<input type="number" class="tankId" style="width: 70px;text-align: center;">';
+    cell2.innerHTML = '<input type="number" class="nozzleId" style="width: 70px;text-align: center;">';
+    cell3.innerHTML = '<input type="number" class="volumeDeciliters" style="width: 150px;text-align: right;">';
+    cell4.innerHTML = '<input type="number" class="cashCents" style="width: 150px;text-align: right;">';
+    cell5.innerHTML = '<input type="text" class="productName" style="width: 70px;text-align: center;">';
+    cell6.innerHTML = '<input type="number" class="invoiceXmlKey" style="width: 390px;text-align: center;">';
+    cell7.innerHTML = '<button onclick="removeRow(this)">-</button>';
+    cell8.innerHTML = '<button onclick="addRow()">+</button>';
+
+    // Add event listener to the transactionType select element
+    var transactionTypeSelect = newRow.querySelector('.transactionType');
+    transactionTypeSelect.addEventListener('change', function() {
+        handleTransactionTypeChange(this);
+    });
+
+    // Initialize the row based on the default transactionType
+    handleTransactionTypeChange(transactionTypeSelect);
+}
+
+
+function removeRow(button) {
+    var table = button.parentNode.parentNode.parentNode; // Get the table element
+    var rowCount = table.rows.length;
+
+    // Check if the number of rows is greater than 2 (header + at least one data row)
+    if (rowCount > 2) {
+        var row = button.parentNode.parentNode;
+        row.parentNode.removeChild(row);
+    } else {
+        console.log("Cannot remove the last row.");
+    }
+}
+
+
+// Function to handle changes in transaction type
+function handleTransactionTypeChange(selectElement) {
+    var row = selectElement.parentNode.parentNode;
+    var tankId = row.querySelector('.tankId');
+    var volumeDeciliters = row.querySelector('.volumeDeciliters');
+    var cashCents = row.querySelector('.cashCents');
+    var productName = row.querySelector('.productName');
+    var invoiceXmlKey = row.querySelector('.invoiceXmlKey');
+    var nozzleId = row.querySelector('.nozzleId');
+
+    // Reset fields to be editable
+    [tankId, volumeDeciliters, cashCents, productName, invoiceXmlKey, nozzleId].forEach(input => {
+        input.disabled = false;
+        input.value = '';
+    });
+
+    switch (selectElement.value) {
+        case '0': // Entrada
+            nozzleId.disabled = true;
+            nozzleId.value = 0;
+            break;
+        case '1': // Venda
+            [invoiceXmlKey, productName].forEach(input => {
+                input.disabled = true;
+                input.value = 0;
+            });
+            break;
+        case '2': // Estoque
+            [invoiceXmlKey, productName, nozzleId, cashCents].forEach(input => {
+                input.disabled = true;
+                input.value = 0;
+            });
+            break;
+    }
+}
+
+function transactionTypeToString(type) {
+    switch (Number(type)) { // Convert type to a number
+        case 0:
+            return "Entrada";
+        case 1:
+            return "Saída";
+        case 2:
+            return "Estoque";
+        default:
+            return "Unknown";
+    }
+}
+
+function populateTable(transactions) {
+    const table = document.getElementById('transactionsTableOutput').getElementsByTagName('tbody')[0];
+    table.innerHTML = ''; // Clear existing rows
+
+    transactions.forEach(function(productTransaction) {
+        productTransaction.transactions.forEach(function(transactionData) {
+            const row = table.insertRow();
+            const cellValues = [
+                productTransaction.referenceDate,
+                transactionTypeToString(transactionData.transactionType),
+                transactionData.tankId,
+                transactionData.nozzleId,
+                transactionData.volumeDeciliters,
+                transactionData.cashCents,
+                transactionData.productName,
+                transactionData.invoiceXmlKey
+            ];
+
+            cellValues.forEach(function(value) {
+                const cell = row.insertCell();
+                cell.textContent = value.toString() || 'N/A'; // Handle null or undefined values
+            });
+        });
+    });
+}
+
+
+// Add an event listener to existing row(s)
+document.querySelectorAll('#transactionsTable .transactionType').forEach(select => {
+    select.addEventListener('change', function() {
+        handleTransactionTypeChange(this);
+    });
+    // Initialize each existing row based on its current transactionType
+    handleTransactionTypeChange(select);
 });
 
-let contract; // Declare contract globally
-
-async function loadContractABI() {
-    const response = await fetch('contractABI.json');
-    const data = await response.json();
-    return data;
-}
-
-async function startApp() {
-    let contractAddress = "0x89c6a06755be8CE794C4994ead34408f3307d55F";
-    const contractABI = await loadContractABI();
-    contract = new web3.eth.Contract(contractABI, contractAddress); // Remove 'let' to use the global variable
-}
-
-async function initApp() {
-    try {
-        await startApp();
-        console.log("Contract is initialized and ready for interactions");
-    } catch (error) {
-        console.error("Error initializing the app:", error);
-        alert("Error initializing the app:", error);
-    }
-}
-
-function addRow(tableName, tableColumns) {
-    const table = document.getElementById(tableName).getElementsByTagName('tbody')[0];
-    const newRow = table.insertRow();
-
-    for (let i = 0; i < (tableColumns - 1); i++) {
-        let cell = newRow.insertCell(i);
-        let input = document.createElement("input");
-        input.type = "text";
-        cell.appendChild(input);
-    }
-
-    // Add delete button in the last cell
-    let deleteCell = newRow.insertCell(tableColumns - 1);
-    let deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "Apagar";
-    deleteButton.onclick = function() {
-        deleteRow(this);
-    };
-    deleteCell.appendChild(deleteButton);
-}
-
-function deleteRow(btn) {
-    let row = btn.parentNode.parentNode;
-    row.parentNode.removeChild(row);
-}
-
-async function productIn() {
-    const referenceDate = document.getElementById("referenceDate").value;
-    const table = document.getElementById("productsInTable").getElementsByTagName('tbody')[0];
-    const rows = table.rows;
-
-    let tankIds = [];
-    let volumeDeciliters = [];
-    let cashCents = [];
-    let productNames = [];
-    let invoiceXmlKeys = [];
-
-    for (let i = 0; i < rows.length; i++) {
-        tankIds.push(parseInt(rows[i].cells[0].getElementsByTagName('input')[0].value));
-        volumeDeciliters.push(parseInt(rows[i].cells[1].getElementsByTagName('input')[0].value));
-        cashCents.push(parseInt(rows[i].cells[2].getElementsByTagName('input')[0].value));
-        productNames.push(rows[i].cells[3].getElementsByTagName('input')[0].value);
-        invoiceXmlKeys.push(parseInt(rows[i].cells[4].getElementsByTagName('input')[0].value));
-    }
-
-    const accounts = await web3.eth.getAccounts();
-    await contract.methods.productIn(referenceDate, tankIds, volumeDeciliters, cashCents, productNames, invoiceXmlKeys)
-        .send({
-            from: accounts[0]
-        });
-}
-
-async function productOut() {
-    const referenceDate = document.getElementById("referenceDate").value;
-    const table = document.getElementById("productsOutTable").getElementsByTagName('tbody')[0];
-    const rows = table.rows;
-
-    let tankIds = [];
-    let nozzleIds = [];
-    let volumeDeciliters = [];
-    let cashCents = [];
-
-    for (let i = 0; i < rows.length; i++) {
-        tankIds.push(parseInt(rows[i].cells[0].getElementsByTagName('input')[0].value));
-        nozzleIds.push(parseInt(rows[i].cells[1].getElementsByTagName('input')[0].value));
-        volumeDeciliters.push(parseInt(rows[i].cells[2].getElementsByTagName('input')[0].value));
-        cashCents.push(parseInt(rows[i].cells[3].getElementsByTagName('input')[0].value));
-    }
-
-    const accounts = await web3.eth.getAccounts();
-    await contract.methods.productOut(referenceDate, tankIds, nozzleIds, volumeDeciliters, cashCents)
-        .send({
-            from: accounts[0]
-        });
-}
-
-async function productStock() {
-    const referenceDate = document.getElementById("referenceDate").value;
-    const table = document.getElementById("productStockTable").getElementsByTagName('tbody')[0];
-    const rows = table.rows;
-
-    let tankIds = [];
-    let volumeDeciliters = [];
-
-    for (let i = 0; i < rows.length; i++) {
-        tankIds.push(parseInt(rows[i].cells[0].getElementsByTagName('input')[0].value));
-        volumeDeciliters.push(parseInt(rows[i].cells[1].getElementsByTagName('input')[0].value));
-    }
-
-    const accounts = await web3.eth.getAccounts();
-    await contract.methods.productStock(referenceDate, tankIds, volumeDeciliters)
-        .send({
-            from: accounts[0]
-        });
-}
-
-
-
-function replacer(key, value) {
-    if (typeof value === 'bigint') {
-        return value.toString();
-    } else {
-        return value;
-    }
-}
-
-async function audit() {
-    console.log('function audit() called');
-    const address = document.getElementById("audit_address").value;
-    if (!web3.utils.isAddress(address)) {
-        alert("Invalid address");
-        return;
-    }
-    const rawData = await contract.methods.audit(address).call();
-    const simplifiedOutput = simplifyData(rawData);
-    console.log(simplifiedOutput);
-    //document.getElementById("auditResult").textContent = JSON.stringify(simplifiedOutput, replacer, 2);
-    // Populate tables with the simplified output
-    populateTables(simplifiedOutput);
-}
-
-function simplifyData(originalData) {
-    const simplifiedData = {};
-
-    for (const key in originalData) {
-        if (Array.isArray(originalData[key])) {
-            simplifiedData[key] = originalData[key].map((item) => {
-                const transformedItem = {};
-
-                for (const prop in item) {
-                    if (!isNaN(prop) || prop === "__length__") {
-                        continue; // Skip indexed properties and "__length__"
-                    }
-                    transformedItem[prop] = item[prop];
-                }
-
-                return transformedItem;
-            });
-        } else {
-            simplifiedData[key] = originalData[key];
+document.addEventListener('DOMContentLoaded', async function() {
+    await connectWallet();
+    document.getElementById('submitTransactionButton').addEventListener('click', async () => {
+        const referenceDate = document.getElementById('referenceDate').value;
+        if (!referenceDate) {
+            alert("Data inválida.");
+            return;
         }
-    }
+        const transactionsData = [];
+        document.querySelectorAll('#transactionsTableInput tr:not(:first-child)').forEach(row => {
+            const transactionType = row.querySelector('.transactionType').value;
+            const tankId = row.querySelector('.tankId').value;
+            const volumeDeciliters = row.querySelector('.volumeDeciliters').value;
+            const cashCents = row.querySelector('.cashCents').value;
+            const productName = row.querySelector('.productName').value;
+            const invoiceXmlKey = row.querySelector('.invoiceXmlKey').value;
+            const nozzleId = row.querySelector('.nozzleId').value;
+            transactionsData.push({
+                transactionType: parseInt(transactionType),
+                tankId: parseInt(tankId),
+                volumeDeciliters: parseInt(volumeDeciliters),
+                cashCents: parseInt(cashCents),
+                productName: productName,
+                invoiceXmlKey: parseInt(invoiceXmlKey),
+                nozzleId: parseInt(nozzleId)
+            });
+        });
 
-    delete simplifiedData["__length__"]; // Remove "__length__" property if it exists at the top level
+        try {
+            const account = (await web3.eth.getAccounts())[0];
+            document.getElementById("errorMessage").textContent = "Conta conectada: " + account;
+            contract.methods.checkRecordExists(referenceDate).call({
+                    from: account
+                })
+                .then(exists => {
+                    if (exists) {
+                        alert("Data já registrada.");
+                    } else {
+                        contract.methods.recordTransaction(referenceDate, transactionsData)
+                            .send({
+                                from: account
+                            })
+                            .then(receipt => {
+                                console.log("Recibo: ", receipt);
+                                document.getElementById("errorMessage").textContent = "Dados enviados. Seu recibo: " + receipt.transactionHash;
+                            })
+                            .catch(err => {
+                                console.error("Transaction error: ", err);
+                                alert("Erro no envio da transação. Contate o suporte ou tente novamente mais tarde.");
+                            });
+                    }
+                })
+                .catch(err => {
+                    console.error("Error checking record existence: ", err);
+                    alert("Verifique se preencheu todos os dados e tente novamente.");
+                });
+        } catch (err) {
+            console.error(err);
+        }
+    });
 
-    return simplifiedData;
-}
+    document.getElementById('readDataButton').addEventListener('click', function() {
+        web3.eth.getAccounts().then(accounts => {
+            // Assuming the first account is the user's account
+            const userAccount = accounts[0];
+            document.getElementById("errorMessage").textContent = "Conta conectada: " + userAccount;
+            return contract.methods.readTransactions().call({
+                from: userAccount
+            });
+        }).then(function(transactions) {
+            console.log(transactions);
+            populateTable(transactions);
+        }).catch(function(error) {
+            console.error("Error reading transactions:", error);
+        });
+    });
 
-function populateTables(data) {
-    const table1 = document.querySelector('#table1 tbody');
-    const table2 = document.querySelector('#table2 tbody');
-    const table3 = document.querySelector('#table3 tbody');
-
-    table1.innerHTML = data["0"].map(item =>
-        `<tr>
-            <td>${item.referenceDate}</td>
-            <td>${item.tankId}</td>
-            <td>${item.volumeDeciliters}</td>
-            <td>${item.cashCents}</td>
-            <td>${item.productName}</td>
-            <td>${item.invoiceXmlKey}</td>
-        </tr>`
-    ).join('');
-
-    table2.innerHTML = data["1"].map(item =>
-        `<tr>
-            <td>${item.referenceDate}</td>
-            <td>${item.tankId}</td>
-            <td>${item.nozzleId}</td>
-            <td>${item.volumeDeciliters}</td>
-            <td>${item.cashCents}</td>
-        </tr>`
-    ).join('');
-
-    table3.innerHTML = data["2"].map(item =>
-        `<tr>
-            <td>${item.referenceDate}</td>
-            <td>${item.tankId}</td>
-            <td>${item.volumeDeciliters}</td>
-        </tr>`
-    ).join('');
-}
+});
